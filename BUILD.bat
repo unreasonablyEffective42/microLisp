f02a068c4f114dd685b839367d309ab62db3891b
Jordan <unreasonablyEffective42@gmail.com>
2025-10-03 13:34:40 -0600

    that should actually fix the windows installer

--- BUILD.bat
+++ BUILD.bat
@@ -1,45 +1,38 @@
 @echo off
 setlocal EnableExtensions
 
-:: --- Locate Java bin folder ---
-echo Checking Java installation...
-
-if defined JAVA_HOME (
-  set "JAVABIN=%JAVA_HOME%\bin"
-) else (
-  for /f "usebackq tokens=*" %%i in (`where java`) do (
-    set "JAVABIN=%%~dpi"
-    goto :foundjava
-  )
+:: --- Locate JDK bin folder ---
+set "JAVABIN="
+for /d %%D in ("C:\Program Files\Java\jdk-*") do (
+    if exist "%%D\bin\jar.exe" (
+        set "JAVABIN=%%D\bin"
+        goto foundjdk
+    )
 )
 
-:foundjava
+:foundjdk
 if not defined JAVABIN (
-  echo Could not determine Java bin path.
-  exit /b 1
+    echo ERROR: Could not find a JDK with jar.exe under C:\Program Files\Java\jdk-*
+    exit /b 1
 )
 
-:: Remove trailing backslash if exists
-if "%JAVABIN:~-1%"=="\" set "JAVABIN=%JAVABIN:~0,-1%"
+echo Using JDK bin: %JAVABIN%
 
-echo Using Java bin: %JAVABIN%
-
-:: Prepend Java bin to PATH for this script only
-set "PATH=%JAVABIN%;%PATH%"
-
-:: --- Build steps ---
+:: Clean + compile
 if exist out rmdir /s /q out
 mkdir out
 
 echo Compiling...
-javac -d out src\*.java
+"%JAVABIN%\javac" -d out src\*.java
 if errorlevel 1 (
   echo Compile failed.
   exit /b 1
 )
 
+copy /Y banner.txt out\
+
 echo Creating JAR...
-jar cfm MicroLisp.jar manifest.mf -C out .
+"%JAVABIN%\jar" cfm MicroLisp.jar manifest.mf -C out .
 if errorlevel 1 (
   echo JAR creation failed.
   exit /b 1
@@ -49,8 +42,8 @@ if errorlevel 1 (
 if not exist "%USERPROFILE%\bin" mkdir "%USERPROFILE%\bin"
 
 :: Copy jar into bin folder so launcher can always find it
-copy /Y "MicroLisp.jar" "%USERPROFILE%\bin\" 
-
+copy /Y "MicroLisp.jar" "%USERPROFILE%\bin\" >nul
+echo Adding microlisp to the path
 (
   echo @echo off
 ) > "%USERPROFILE%\bin\microlisp.bat"
@@ -58,5 +51,20 @@ copy /Y "MicroLisp.jar" "%USERPROFILE%\bin\"
 (
   echo java -jar "%%~dp0MicroLisp.jar" %%*
 ) >> "%USERPROFILE%\bin\microlisp.bat"
+
+:: --- Safely add %USERPROFILE%\bin to PATH without truncating ---
+reg query "HKCU\Environment" /v PATH | find /i "%USERPROFILE%\bin" >nul
+if errorlevel 1 (
+    for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v PATH ^| find /i "PATH"') do (
+        set "UserPath=%%b"
+    )
+    if not defined UserPath (
+        setx PATH "%USERPROFILE%\bin"
+    ) else (
+        setx PATH "%UserPath%;%USERPROFILE%\bin"
+    )
+)
+
+echo MicroLisp installed, open a new terminal and enter 'microlisp' to use
 
 endlocal
